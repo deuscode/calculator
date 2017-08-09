5b7115c2618a8f9686b097f26f61e1ef14960cf3
deuscode <deuscode87@gmail.com>
2017-08-09 01:09:49 -0400

    add power functions

--- js/main.js
+++ js/main.js
@@ -1,14 +1,26 @@
-var displayBox = document.getElementById('display');
+var displayBox = document.getElementById("display");
 
 function toDisplay(x) {
-    displayBox.value += x;
-    if (x === 'C') {
-        displayBox.value = '';
-    }
+  displayBox.value += x;
+  if (x === "C") {
+    displayBox.value = "";
+  }
 }
 
+function squareNum() {
+  var x = displayBox.value;
+  displayBox.value = x * x;
+}
+
+function cubeNum() {
+  var x = displayBox.value;
+  displayBox.value = x * x * x;
+}
+
+function fractionNum() {}
+
 function equateAnswer() {
-    x = displayBox.value;
-    x = eval(x);
-    displayBox.value = x;  
+  var x = displayBox.value;
+  x = eval(x);
+  displayBox.value = x;
 }
