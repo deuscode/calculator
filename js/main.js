7264c6299f1b383fa8d49d58a19f9b50c73b37e7
deuscode <deuscode87@gmail.com>
2017-08-09 01:37:04 -0400

    add additional functions

--- js/main.js
+++ js/main.js
@@ -1,26 +1,39 @@
 var displayBox = document.getElementById("display");
 
 function toDisplay(x) {
-  displayBox.value += x;
-  if (x === "C") {
-    displayBox.value = "";
+    displayBox.value += x;
+    if (x === "C") {
+        displayBox.value = "";
   }
 }
 
+function percentageNum() {
+    var x = displayBox.value;
+    displayBox.value = x / 100;
+}
+
+function radicalNum() {
+    var x = displayBox.value;
+    displayBox.value = Math.sqrt(x);
+}
+
 function squareNum() {
-  var x = displayBox.value;
-  displayBox.value = x * x;
+    var x = displayBox.value;
+    displayBox.value = x * x;
 }
 
 function cubeNum() {
-  var x = displayBox.value;
-  displayBox.value = x * x * x;
+    var x = displayBox.value;
+    displayBox.value = x * x * x;
 }
 
-function fractionNum() {}
+function fractionNum() {
+    var x = displayBox.value;
+    displayBox.value = 1 / x;
+}
 
 function equateAnswer() {
-  var x = displayBox.value;
-  x = eval(x);
-  displayBox.value = x;
+    var x = displayBox.value;
+    x = eval(x);
+    displayBox.value = x;
 }
