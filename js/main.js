abbb9f6d3c1f5222c22985c88a7a0130cb351628
Peter Kay <contact@kaypeter.com>
2017-08-10 15:24:49 -0400

    add comments

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 var displayBox = document.getElementById("display");
 
+// send button inputs to calculator display
+// clear function when pressing 'C'
 function toDisplay(x) {
     displayBox.value += x;
     if (x === "C") {
@@ -7,31 +9,37 @@ function toDisplay(x) {
   }
 }
 
+// find percentage of current number on display
 function percentageNum() {
     var x = displayBox.value;
     displayBox.value = x / 100;
 }
 
+// find square root of current number on display
 function radicalNum() {
     var x = displayBox.value;
     displayBox.value = Math.sqrt(x);
 }
 
+// square the current number on display
 function squareNum() {
     var x = displayBox.value;
     displayBox.value = x * x;
 }
 
+// cube the current number on display
 function cubeNum() {
     var x = displayBox.value;
     displayBox.value = x * x * x;
 }
 
+// find the fraction of the current number on display
 function fractionNum() {
     var x = displayBox.value;
     displayBox.value = 1 / x;
 }
 
+// find the answer to the current problem
 function equateAnswer() {
     var x = displayBox.value;
     x = eval(x);
